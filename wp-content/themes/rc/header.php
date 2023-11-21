1d0016c5f7e4331f84d3e2da616a57803e256bf6
Tyler J. Perry <iamtylerjordan@gmail.com>
2023-11-21 15:06:33 -0500

    feat: adding desktop menu styles

--- wp-content/themes/rc/header.php
+++ wp-content/themes/rc/header.php
@@ -51,7 +51,7 @@
 			</div>
 		</div>
 		<div class="header--desktop d-none d-md-block">
-			<div class="header__nav-wrapper container flex">
+			<div class="header__nav-wrapper container-lg d-md-flex justify-content-md-between align-items-md-center">
 				<div class="logo">
 					<h1 class="screen-reader"><?php bloginfo( 'name' ); ?></h1>
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php include('img/rc-logo.svg');?></a>
@@ -61,7 +61,7 @@
 						array(
 							'theme_location' => 'menu-1',
 							'menu_id'        => 'primary-menu',
-							'container'	=> ''
+							'container'	=> '',
 						)
 					);?>
 				</div>
