d78bd0ba3cc534afb390c71177549b85088405fd
Tyler J. Perry <iamtylerjordan@gmail.com>
2023-12-02 23:29:50 -0500

    wip: styling hero

--- wp-content/themes/rc/template-parts/hero.php
+++ wp-content/themes/rc/template-parts/hero.php
@@ -5,16 +5,16 @@
  * 
  */
 $text = get_sub_field('text');
-//$links = get_sub_field('links');
+$image = get_sub_field('image');
 ?>
 
-<div class="hero">
-    <div class="hero__content container-lg d-flex-md">
+<div class="hero d-flex align-items-center">
+    <div class="hero__content container-lg d-md-flex align-items-md-center justify-items-md-between">
         <?php if(!empty($text)):?>
             <div class="hero__content-text col-md">
                 <?php echo $text;?>
                 <?php if(have_rows('button_group')):?>
-                    <div class="buttong-group d-flex">
+                    <div class="button-group d-flex">
                         <?php while(have_rows('button_group')): the_row();
                             $link = get_sub_field('link');
                         ?>
@@ -29,7 +29,18 @@ $text = get_sub_field('text');
                 <?php endif;?>
             </div>
             <div class="hero__content-media col-md">
-                <>
+                <?php if(!empty($image)):
+                    $img_src = $image['url'];
+                    $img_alt_text = $image['alt'];
+                ?>
+                    <div class="media__img">
+                        <img src="<?php $img_src;?>" alt="<?php $img_alt_text;?>"/>
+                    </div>
+                <?php else:?>
+                    <div class="media__img">
+                        <img src="http://placehold.it/400"/>
+                    </div>
+                <?php endif;?>
             </div>
         <?php endif;?>
     </div>
