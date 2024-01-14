a107200ed43760ebe322fa2f451d7ce3f49da337
Tyler J. Perry <iamtylerjordan@gmail.com>
2024-01-14 00:58:30 -0500

    wip: adding content card component

---
+++ wp-content/themes/rc/template-parts/component-content-card.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ * Component Name: Content Card 
+ * 
+ * 
+ */
+$image = get_sub_field('image');
+$title = get_sub_field('title');
+$desc = get_sub_field('description');
+?>
--- wp-content/themes/rc/template-parts/component-testimonials.php
+++ wp-content/themes/rc/template-parts/component-testimonials.php
@@ -14,7 +14,7 @@ $post_query = new WP_Query($args);
 ?>
 <?php if($post_query -> have_posts()): $count = 0;?>
     <div class="archive content-row">
-        <div class="archive__inner container-lg">
+        <div class="archive__inner archive__inner--sm container-lg">
             <?php while($post_query -> have_posts()): $post_query -> the_post();
                 $count++;
                 $author_name = get_field('author_name');
