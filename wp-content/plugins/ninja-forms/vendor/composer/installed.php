91246e2ed2c9f95ca50c0377db5b59d9a74664c3
Tyler J. Perry <iamtylerjordan@gmail.com>
2024-02-05 10:01:21 -0500

    wp-update

---
+++ wp-content/plugins/ninja-forms/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'saturday-drive/ninja-forms',
+        'pretty_version' => 'dev-eb493ebe9b91d8acc1444fc81de62a7be32edb0f',
+        'version' => 'dev-eb493ebe9b91d8acc1444fc81de62a7be32edb0f',
+        'reference' => 'eb493ebe9b91d8acc1444fc81de62a7be32edb0f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'saturday-drive/ninja-forms' => array(
+            'pretty_version' => 'dev-eb493ebe9b91d8acc1444fc81de62a7be32edb0f',
+            'version' => 'dev-eb493ebe9b91d8acc1444fc81de62a7be32edb0f',
+            'reference' => 'eb493ebe9b91d8acc1444fc81de62a7be32edb0f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
